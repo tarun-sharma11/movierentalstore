89e7bdd1769cb547e472e36ef446f6f81b5b5fe7
Tarun <ts137137@gmail.com>
2020-11-25 16:26:27 +0530

    app file fixed

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
-const cusapp = express();
+
 const seed = require("./seedDB");
 const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const passport = require("passport");
-// const userPassport = require("passport");
+
 const router = express.Router();
 const initializePassport = require("./passportConfig");
 initializePassport(passport);
@@ -23,26 +23,15 @@ app.use(
 		saveUninitialized: false
 	})
 );
-// cusapp.use(express.json());
-// cusapp.use(bodyParser.urlencoded({extended:true}));
-// cusapp.use(
-// 	session ({
-// 		secret: "This backend was created by Tarun",
-// 		resave : false,
-// 		saveUninitialized: false
-// 	})
-// );
+
 router.use(express.static(__dirname+"./public/"));
 app.use(passport.initialize());
 app.use(passport.session());
-// cusapp.use(userPassport.initialize());
-// cusapp.use(userPassport.session());        
+       
 app.set("view engine","ejs");
 app.use(express.static("public"));   
 app.use(methodOverride('_method'));
-// cusapp.set("view engine","ejs");
-// cusapp.use(express.static("public"));   
-// cusapp.use(methodOverride('_method'));
+
 // Seeding
 seed.execute();
 // Routes
